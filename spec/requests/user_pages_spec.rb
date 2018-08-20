6e75d306f5a38f0e5e290b1ae886213c86d2f017
Mohamed Ibrahim <mohamed.ibrahim@freshworks.com>
2018-08-20 13:12:38 +0530

    fixed typo

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "User Pages" do
   subject {page}
   describe "signup page" do
-    before {visit signup_page}
+    before {visit signup_path}
     it {should have_selector('h1', text: 'Signup')}
     it {should have_selector('title', text: full_title('Sign Up'))}
   end
