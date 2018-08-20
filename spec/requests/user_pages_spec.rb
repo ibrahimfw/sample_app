2746c78c8401583cc6159b3892097c16a4523791
Mohamed Ibrahim <mohamed.ibrahim@freshworks.com>
2018-08-20 20:18:12 +0530

    fixed test case

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,7 +18,7 @@ describe "User Pages" do
 
   describe "signup" do
     before {visit signup_path}
-    let(:submit) { "Create my account" }
+    let(:submit) {"Create my account"}
     describe "with invalid information" do
       it "should not create a user" do
         expect {click_button submit}.not_to change(User, :count)
@@ -27,10 +27,10 @@ describe "User Pages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name", "Example User"
-        fill_in "Email", "user@example.com"
-        fill_in "Password", "foobar"
-        fill_in "Confirmation", "foobar"
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
       end
       it "should create a user" do
         expect {click_button submit}.to change(User, :count).by(1)
